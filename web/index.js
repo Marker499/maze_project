2d1b8653c255feabef9b4147a37e8162b17fdccc
Alex Chernov <nak4442@gmail.com>
2026-02-04 19:38:33 +0300

    Отображение лабиринта

--- web/index.js
+++ web/index.js
@@ -4,15 +4,35 @@ const seedInput = document.querySelector('#seed');
 
 const button = document.querySelector('.generate-button');
 
+const mazeElement = document.querySelector('#maze');
+
 const getMaze = async (width, height, seed) => {
     return await eel.generate(width, height, seed) ();
+}
+
+const renderMaze = (maze) => {
+    mazeElement.innerHTML = '';
+    maze.forEach(row => {
+        const rowElement = document.createElement('div');
+        rowElement.classList.add('row');
+        row.forEach(cell => {
+            const cellElement = document.createElement('div');
+            cellElement.classList.add('cell');
+            if(cell === 1){
+                cellElement.classList.add('wall');
+            }
+            rowElement.appendChild(cellElement);
+        })
+        mazeElement.appendChild(rowElement);
+    })
 }
 
 button.addEventListener('click', () => {
     const width = Number(widthInput.value);
     const height = Number(heightInput.value);
     const seed = Number(seedInput.value);
-    console.log(width, height, seed);
-    getMaze(width, height, seed).then(maze => console.log(maze))
 
+    getMaze(width, height, seed).then(maze => {
+        renderMaze(maze);
+    })
 })
