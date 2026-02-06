66d211be8ca6d2e9f899c995479e9f9aa7ac770e
Alex Chernov <nak4442@gmail.com>
2026-02-06 18:09:19 +0300

    Генерация 3D модели с интерфейса

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from core import EllerMazeGenerator, Maze3DExporter
 import eel
 #
@@ -17,12 +18,37 @@ import eel
 # exporter.export_to_stl('7.stl', True)
 eel.init('web')
 
+maze_generator = None
+exporter = None
 
 @eel.expose
-def generate(width, height, seed):
+def generate(width: int, height: int, seed: int):
+    global maze_generator
     maze_generator = EllerMazeGenerator(width, height, seed)
     maze_generator.generate()
     return maze_generator.get_maze_matrix()
+
+
+
+@eel.expose
+def export_to_stl(
+        file_name: str, 
+        cell_size: float = 10,
+        wall_height: float = 5,
+        wall_thickness: float = 1,
+        floor_thickness: float = 1,
+        is_ascii: bool = False
+    ):
+    global maze_generator, exporter
+    if maze_generator is None:
+        return False
+    if len(file_name) == 0:
+        file_name = f"maze_{round(time.time())}.stl"
+    
+    exporter = Maze3DExporter(maze_generator, cell_size, wall_height, wall_thickness, floor_thickness)
+    exporter.generate_3D_model()
+    exporter.export_to_stl(file_name, is_ascii)
+    return True
 
 
 def main():
--- web/index.js
+++ web/index.js
@@ -1,13 +1,44 @@
+const generateSection = document.querySelector(".generate");
+const exportSection = document.querySelector(".export");
+
 const widthInput = document.querySelector('#width');
 const heightInput = document.querySelector('#height');
 const seedInput = document.querySelector('#seed');
 
-const button = document.querySelector('.generate-button');
+const cellSizeInput = document.querySelector('#cell_size');
+const wallHeightInput = document.querySelector('#wall_height');
+const wallThicknessInput = document.querySelector('#wall_thickness');
+const floorThicknessInput = document.querySelector('#floor_thickness');
+
+const isAsciiBinaryRadio = document.querySelector("#binary");
+const isAsciiAsciiRadio = document.querySelector("#ascii");
+
+const generateButton = document.querySelector('#generate-button');
+const regenerateButton = document.querySelector('#regenerate-button');
+const exportButton = document.querySelector("#export-button");
 
 const mazeElement = document.querySelector('#maze');
 
 const getMaze = async (width, height, seed) => {
     return await eel.generate(width, height, seed) ();
+}
+
+const exportToStl = async (
+        file_name, 
+        cell_size,
+        wall_height,
+        wall_thickness,
+        floor_thickness,
+        is_ascii
+    ) => {
+    return await eel.export_to_stl(
+        file_name, 
+        cell_size,
+        wall_height,
+        wall_thickness,
+        floor_thickness,
+        is_ascii
+    ) ();
 }
 
 const renderMaze = (maze) => {
@@ -27,12 +58,37 @@ const renderMaze = (maze) => {
     })
 }
 
-button.addEventListener('click', () => {
+generateButton.addEventListener('click', () => {
     const width = Number(widthInput.value);
     const height = Number(heightInput.value);
     const seed = Number(seedInput.value);
 
     getMaze(width, height, seed).then(maze => {
         renderMaze(maze);
+        generateSection.classList.toggle("hidden");
+        exportSection.classList.toggle("hidden");
     })
+})
+
+regenerateButton.addEventListener('click', () => {
+    generateSection.classList.toggle("hidden");
+    exportSection.classList.toggle("hidden");
+})
+
+exportButton.addEventListener('click', () => {
+    const cellSize = Number(cellSizeInput.value);
+    const wallHeight = Number(wallHeightInput.value);
+    const wallThickness = Number(wallThicknessInput.value);
+    const floorThickness = Number(floorThicknessInput.value);
+
+    const isAscii = isAsciiAsciiRadio.checked;
+    
+    exportToStl(
+        "", 
+        cellSize, 
+        wallHeight, 
+        wallThickness, 
+        floorThickness, 
+        isAscii
+    ).then(res => (res) ? alert("Лабиринт успешно сгенерирован") : alert("Ошибка при генерации лабиринта"));
 })
